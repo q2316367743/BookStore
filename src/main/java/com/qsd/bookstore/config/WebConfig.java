b2c6d067e77830b12d33fb5a6629fc60a978002c
浅梦凉城 <m17762618644@163.com>
2020-06-15 14:26:49 +0800

    前后端分离

--- src/main/java/com/qsd/bookstore/config/WebConfig.java
+++ src/main/java/com/qsd/bookstore/config/WebConfig.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -56,5 +57,16 @@ public class WebConfig implements WebMvcConfigurer {
 				.addPathPatterns(need)
 				.excludePathPatterns(dis);
 	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedHeaders("*")
+                .allowedMethods("*")
+                .maxAge(1800)
+                .allowedOrigins("*");
+	}
+	
+	
 	
 }
